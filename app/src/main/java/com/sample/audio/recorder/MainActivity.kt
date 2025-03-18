e3f6a695c3ec5cbf92b2e88e67498a538802ce63
inseon Yun <yuninseon@naver.com>
2025-03-18 16:27:26 +0900

    feat: Implement Main Screen view

--- app/src/main/java/com/sample/audio/recorder/MainActivity.kt
+++ app/src/main/java/com/sample/audio/recorder/MainActivity.kt
@@ -3,7 +3,9 @@ package com.sample.audio.recorder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sample.audio.recorder.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
---
+++ app/src/main/java/com/sample/audio/recorder/application/SampleApplication.kt
@@ -0,0 +1,7 @@
+package com.sample.audio.recorder.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SampleApplication : Application()
