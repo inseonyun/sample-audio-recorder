47c73fc46c85b89239a4cf170890341cf629c5a1
inseon Yun <yuninseon@naver.com>
2025-03-18 16:36:34 +0900

    chore: Init add Kotlin serialization dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ksp)
 }
