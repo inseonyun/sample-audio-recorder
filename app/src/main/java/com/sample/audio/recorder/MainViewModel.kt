32186a3c4a48c9e024bef802619ae06c501aadc2
inseon Yun <yuninseon@naver.com>
2025-03-18 16:29:49 +0900

    feat: Create MainViewModel

---
+++ app/src/main/java/com/sample/audio/recorder/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.sample.audio.recorder
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewModel : ViewModel() {
+
+    fun create() {
+
+    }
+
+    fun start() {
+
+    }
+
+    fun end() {
+
+    }
+
+    fun cancel() {
+
+    }
+}
