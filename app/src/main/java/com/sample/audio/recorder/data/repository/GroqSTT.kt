d16019a8adc659df43c44e43171f8758f8c5f827
inseon Yun <yuninseon@naver.com>
2025-03-19 09:13:13 +0900

    feat: Implement start function for GroqSTT

--- app/src/main/java/com/sample/audio/recorder/data/repository/GroqSTT.kt
+++ app/src/main/java/com/sample/audio/recorder/data/repository/GroqSTT.kt
@@ -6,7 +6,13 @@ import android.media.MediaRecorder
 import android.util.Log
 import com.sample.audio.recorder.model.STTState
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import java.io.File
 import javax.inject.Inject
 
@@ -16,6 +22,9 @@ class GroqSTT @Inject constructor(
     private val apiCallback: suspend (File) -> Unit
 ) : VoiceRecorder(state) {
     private var mediaRecorder: MediaRecorder? = null
+
+    private var recordingJob: Job? = null
+    private var silenceJob: Job? = null
 
     @SuppressLint("NewApi")
     override suspend fun create() {
@@ -35,8 +44,41 @@ class GroqSTT @Inject constructor(
     private fun getOutputFile(): File = File.createTempFile(FILE_NAME, FILE_TYPE, context.cacheDir)
 
     override suspend fun start() {
-        TODO("Not yet implemented")
+        try {
+            mediaRecorder?.let {
+                it.prepare()
+                it.start()
+            }
+            recordingJob = CoroutineScope(Dispatchers.IO).launch {
+                delay(MAX_RECORDING_TIME) // Recording to MAX TIME
+                end()
+            }
+            detectSilence()
+        } catch (e: Exception) {
+            Log.e("VoiceRecorder", "MediaRecorder Start ERROR: ${e.message}")
+            state.send(STTState.Error(e.message.toString()))
+        }
     }
+
+    private fun detectSilence() {
+        silenceJob = CoroutineScope(Dispatchers.IO).launch {
+            var lastAmplitude = getAmplitude()
+            while (isActive) {
+                delay(500)
+                val currentAmplitude = getAmplitude()
+                if (currentAmplitude < 2000 && lastAmplitude < 2000) {
+                    delay(1000)
+                    if (getAmplitude() < 2000) {
+                        end()
+                        break
+                    }
+                }
+                lastAmplitude = currentAmplitude
+            }
+        }
+    }
+
+    private fun getAmplitude(): Int = mediaRecorder?.maxAmplitude ?: 0
 
     override fun cancel() {
         TODO("Not yet implemented")
@@ -45,5 +87,7 @@ class GroqSTT @Inject constructor(
     companion object {
         private const val FILE_NAME = "AUDIO_RECORD"
         private const val FILE_TYPE = ".wav"
+
+        private const val MAX_RECORDING_TIME = 15000L
     }
 }
